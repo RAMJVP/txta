87892e021437e060a33a93a90a1e9ee4108123a9
Jeevan Patel <jeevan.v.patel@gmail.com>
2024-11-24 00:00:53 +0530

    gog

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os
 from fastapi import FastAPI, HTTPException, Form
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-import pyttsx3
 import io
+
+from gtts import gTTS
+
+
 
 app = FastAPI()
 
@@ -19,30 +22,23 @@ app.add_middleware(
 # Get the project root folder
 project_root = os.path.dirname(os.path.abspath(__file__))
 
+
+
+
+
 @app.post("/generate-audio/")
 async def generate_audio(text: str = Form(...)):
+    if not text.strip():
+        raise HTTPException(status_code=400, detail="Text is required")
     try:
-        if not text.strip():
-            raise HTTPException(status_code=400, detail="Text is required")
-
-        # Initialize pyttsx3 for text-to-speech
-        engine = pyttsx3.init()
-
-        # Path to save the audio file in the project root
-        output_file_path = os.path.join(project_root, "output.mp3")
-
-        # Save speech to the output.mp3 file
-        engine.save_to_file(text, output_file_path)
-        engine.runAndWait()
-
-        # Read the audio file content
-        with open(output_file_path, "rb") as audio_file:
-            audio_content = audio_file.read()
-
-        # Return the audio as a downloadable file
-        return StreamingResponse(io.BytesIO(audio_content), media_type="audio/mp3", headers={
-            "Content-Disposition": f"attachment; filename=output.mp3"
+        tts = gTTS(text)
+        audio_buffer = io.BytesIO()
+        tts.write_to_fp(audio_buffer)
+        audio_buffer.seek(0)
+        return StreamingResponse(audio_buffer, media_type="audio/mpeg", headers={
+            "Content-Disposition": "attachment; filename=output.mp3"
         })
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
