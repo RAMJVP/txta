f1f10f70d3b3b332598f12164d7c470cc8eb4597
Jeevan Patel <jeevan.v.patel@gmail.com>
2024-11-27 23:07:10 +0530

    i-t

--- ocr_service.py
+++ ocr_service.py
@@ -2,6 +2,15 @@ import pytesseract
 from PIL import Image
 from fastapi import UploadFile
 
+import pytesseract
+import shutil
+
+tesseract_path = shutil.which("tesseract")
+print(f"Tesseract Path: {tesseract_path}")
+
+
+
+
 def extract_text_from_image(uploaded_file: UploadFile) -> str:
     try:
         # Read the uploaded file as an image
